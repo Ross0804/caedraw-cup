215f1b259943c09070c3de433c57b7eedddfec49
Ross0804 <rossbeale2@gmail.com>
2023-03-19 16:01:29 +0000

    Context Changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,17 @@
+<script>
+    import {createClient, setContextClient} from '@urql/svelte'
+    const client = createClient({
+        url: 'https://graphql.fauna.com/graphql',
+        fetchOptions: () => {
+            const token = import.meta.env.VITE_PUBLIC_FAUNA_KEY;
+            return {
+                headers: { authorization: token ? `Bearer ${token}` : '' },
+            };
+        }
+    })
+    setContextClient(client)
+</script>
+
 <svelte:head>
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
 </svelte:head>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,17 +1,5 @@
 <script>
-    import {createClient, setContextClient, queryStore, gql, getContextClient} from '@urql/svelte'
-
-    const client = createClient({
-        url: 'https://graphql.fauna.com/graphql',
-        fetchOptions: () => {
-            const token = import.meta.env.VITE_PUBLIC_FAUNA_KEY;
-            return {
-                headers: { authorization: token ? `Bearer ${token}` : '' },
-            };
-        }
-    })
-
-    setContextClient(client)
+    import {queryStore, gql, getContextClient} from '@urql/svelte'
 
     const housePoints = queryStore({
         client: getContextClient(),
